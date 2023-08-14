16ec01e4e762a3e6a5922a0a3d474bcda79bbd01
rousong <recall131477@gmail.com>
2023-08-14 17:52:50 +0800

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
 import App from './App.vue';
 import router from './router';
 
@@ -6,6 +8,9 @@ import '@/assets/scss/tailwind.scss';
 import '@/assets/scss/app.scss';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
+
 app.mount('#app');
