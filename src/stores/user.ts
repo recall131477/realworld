911fd01a1f9063f9e2d92c4d609e9bcf792b99c2
rousong <recall131477@gmail.com>
2023-08-14 22:38:00 +0800

    modify: adjustment user store option API changed to composition API

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,36 +1,38 @@
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
-
 import type { UserInfo } from '@/types';
-
 import { storage } from '@/utils/storage';
-
 import { getUser } from '@/api';
 
 const userStorage = storage('user');
 
-export const useUserStore = defineStore('user', {
-  state: () => ({
-    userInfo: null as UserInfo | null,
-  }),
-  getters: {
-    isLoggedIn(): boolean {
-      return !!this.userInfo;
-    },
-  },
-  actions: {
-    setUser(user: UserInfo) {
-      this.userInfo = user;
-      userStorage.set(user.token);
-    },
-    removeUser() {
-      this.userInfo = null;
-      userStorage.remove();
-    },
-    async verifyAuth() {
-      if (!this.userInfo && userStorage.get()) {
-        const res = await getUser();
-        this.setUser(res.user);
-      }
-    },
-  },
+export const useUserStore = defineStore('user', () => {
+  const userInfo = ref<UserInfo | null>(null);
+
+  const isLoggedIn = computed(() => !!userInfo.value);
+
+  const setUser = (user: UserInfo) => {
+    userInfo.value = user;
+    userStorage.set(user.token);
+  };
+
+  const removeUser = () => {
+    userInfo.value = null;
+    userStorage.remove();
+  };
+
+  const verifyAuth = async () => {
+    if (!userInfo.value && userStorage.get()) {
+      const res = await getUser();
+      setUser(res.user);
+    }
+  };
+
+  return {
+    userInfo,
+    isLoggedIn,
+    setUser,
+    removeUser,
+    verifyAuth,
+  };
 });
