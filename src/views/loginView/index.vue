aeab0a7a37f08f14c4a71775f255e2bf70658898
rousong <recall131477@gmail.com>
2023-08-15 16:48:45 +0800

    feat: add login and concatenated login API

--- src/views/loginView/index.vue
+++ src/views/loginView/index.vue
@@ -4,16 +4,51 @@ export default {
 };
 </script>
 
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import type { User } from '@/types';
+import type { ErrorObject } from '@/types/error';
+import { useUserStore } from '@/stores/user';
+import { login } from '@/api';
+
+const router = useRouter();
+
+const userStore = useUserStore();
+const { setUser } = userStore;
+
+const user = ref<User>({
+  email: '',
+  password: '',
+});
+
+const errors = ref<ErrorObject>({});
+
+const handleLogin = async () => {
+  try {
+    const res = await login({ user: user.value });
+    setUser(res.user);
+    router.push({ name: 'global-feed' });
+  } catch (error) {
+    errors.value = (error as any).errors;
+  }
+};
+</script>
+
 <template>
   <div class="pt-6">
     <div class="mx-auto max-w-[1140px] px-[15px]">
       <div class="md:mx-auto md:w-1/2">
         <div class="mb-4 text-center">
           <h1 class="text-[40px]">Sign up</h1>
-          <router-link :to="{ name: 'register' }" class="text-primary">Need an account?</router-link>
+          <router-link :to="{ name: 'register' }" class="text-primary"
+            >Need an account?</router-link
+          >
         </div>
-        <ul class="mb-4 list-disc pl-10 font-bold text-danger">
-          <li>error message</li>
+        <ul class="mb-4 list-disc pl-10 font-bold text-danger" v-if="errors">
+          <li v-for="(error, field) in errors" :key="field">
+            {{ field }} {{ error ? error[0] : '' }}
+          </li>
         </ul>
         <form>
           <fieldset class="space-y-4">
@@ -25,6 +60,7 @@ export default {
                 autocomplete="email"
                 class="border-black/15 block w-full rounded border bg-white px-6 py-3 text-xl leading-tight text-[#55595c] outline-none placeholder:text-[#999] disabled:pointer-events-none disabled:bg-[#eceeef]"
                 placeholder="Email"
+                v-model="user.email"
               />
             </fieldset>
             <fieldset>
@@ -35,12 +71,14 @@ export default {
                 autocomplete="current-password"
                 class="border-black/15 block w-full rounded border bg-white px-6 py-3 text-xl leading-tight text-[#55595c] outline-none placeholder:text-[#999] disabled:pointer-events-none disabled:bg-[#eceeef]"
                 placeholder="Password"
+                v-model="user.password"
               />
             </fieldset>
             <div class="text-right">
               <button
                 type="button"
                 class="inline-block rounded bg-primary px-6 py-3 text-xl leading-tight text-white duration-300 hover:bg-primary-dark"
+                @click="handleLogin"
               >
                 Sign up
               </button>
