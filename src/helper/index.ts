e153d2fb048790638fe079bbfe0a6f52e8d65227
rousong <recall131477@gmail.com>
2023-08-17 17:24:59 +0800

    feat: add format date methods

---
+++ src/helper/index.ts
@@ -0,0 +1,15 @@
+export const pageToOffset = (page: number = 1, limit: number = 10) => {
+  const offset = (page - 1) * limit;
+  return {
+    limit,
+    offset,
+  };
+};
+
+export const formatDate = (data: string): string => {
+  return new Date(data).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+};
