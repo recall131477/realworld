cb92dc210f90890f181d3a4480b5387e4d17cf25
rousong <recall131477@gmail.com>
2023-08-25 19:35:28 +0800

    modify: adjustment mobile menu

--- src/components/layout/headerLayout/index.vue
+++ src/components/layout/headerLayout/index.vue
@@ -5,16 +5,26 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useUserStore } from '@/stores/user';
+
+const route = useRoute();
 
 const userStore = useUserStore();
 const { userInfo, isLoggedIn } = storeToRefs(userStore);
 const { removeUser } = userStore;
 
+const isMenuOpen = ref(false);
+
 const logout = () => {
   removeUser();
 };
+
+watch(route, () => {
+  isMenuOpen.value = false;
+});
 </script>
 
 <template>
@@ -27,12 +37,16 @@ const logout = () => {
           >conduit</router-link
         >
         <ul
-          class="fixed left-0 top-14 w-full bg-white md:static md:flex md:w-auto md:gap-x-4"
+          class="fixed left-0 top-14 w-full bg-white duration-300 md:visible md:static md:flex md:w-auto md:gap-x-4 md:opacity-100"
+          :class="isMenuOpen ? 'visible opacity-100' : 'invisible opacity-0'"
         >
           <li class="border-t border-black/10 md:border-0">
             <router-link
               :to="{ name: 'global-feed' }"
-              class="block px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+              class="block px-[15px] py-2.5 duration-300 hover:text-black/60 md:p-0"
+              :class="
+                route.name !== 'global-feed' ? 'text-black/30' : 'text-black/80'
+              "
               >Home
             </router-link>
           </li>
@@ -40,14 +54,20 @@ const logout = () => {
             <li class="border-t border-black/10 md:border-0">
               <router-link
                 :to="{ name: 'login' }"
-                class="block px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+                class="block px-[15px] py-2.5 duration-300 hover:text-black/60 md:p-0"
+                :class="
+                  route.name !== 'login' ? 'text-black/30' : 'text-black/80'
+                "
                 >Sign in</router-link
               >
             </li>
             <li class="border-t border-black/10 md:border-0">
               <router-link
                 :to="{ name: 'register' }"
-                class="block px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+                class="block px-[15px] py-2.5 duration-300 hover:text-black/60 md:p-0"
+                :class="
+                  route.name !== 'register' ? 'text-black/30' : 'text-black/80'
+                "
                 >Sign up</router-link
               >
             </li>
@@ -56,14 +76,22 @@ const logout = () => {
             <li class="border-t border-black/10 md:border-0">
               <router-link
                 :to="{ name: 'create-article' }"
-                class="block px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+                class="block px-[15px] py-2.5 duration-300 hover:text-black/60 md:p-0"
+                :class="
+                  route.name !== 'create-article'
+                    ? 'text-black/30'
+                    : 'text-black/80'
+                "
                 >New Article</router-link
               >
             </li>
             <li class="border-t border-black/10 md:border-0">
               <router-link
                 :to="{ name: 'settings' }"
-                class="block px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+                class="block px-[15px] py-2.5 duration-300 hover:text-black/60 md:p-0"
+                :class="
+                  route.name !== 'settings' ? 'text-black/30' : 'text-black/80'
+                "
               >
                 <i class="ion-gear-a"></i>&nbsp;Settings
               </router-link>
@@ -76,7 +104,10 @@ const logout = () => {
                     username: userInfo?.username,
                   },
                 }"
-                class="flex items-center gap-x-1 px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+                class="flex items-center gap-x-1 px-[15px] py-2.5 duration-300 hover:text-black/60 md:p-0"
+                :class="
+                  route.name !== 'profile' ? 'text-black/30' : 'text-black/80'
+                "
               >
                 <img
                   :src="userInfo?.image"
@@ -89,22 +120,33 @@ const logout = () => {
             <li class="border-t border-black/10 md:border-0">
               <router-link
                 :to="{ name: 'global-feed' }"
-                class="block px-[15px] py-2.5 text-black/80 duration-300 hover:text-black/60 md:p-0"
+                class="block px-[15px] py-2.5 text-black/30 duration-300 hover:text-black/60 md:p-0"
                 @click="logout"
                 >Sign out
               </router-link>
             </li>
           </template>
         </ul>
-        <button type="button" class="relative h-10 w-10 md:hidden">
+        <button
+          type="button"
+          class="relative h-10 w-10 md:hidden"
+          @click="isMenuOpen = !isMenuOpen"
+        >
           <span
-            class="absolute left-1/2 top-3 h-0.5 w-6 -translate-x-1/2 bg-[#333333]"
+            class="absolute left-1/2 h-0.5 w-6 -translate-x-1/2 bg-[#333] duration-300"
+            :class="
+              isMenuOpen ? 'top-1/2  -translate-y-1/2 rotate-45' : 'top-3'
+            "
           ></span>
           <span
-            class="absolute left-1/2 top-1/2 h-0.5 w-6 -translate-x-1/2 -translate-y-1/2 bg-[#333333]"
+            class="absolute left-1/2 top-1/2 h-0.5 w-6 -translate-x-1/2 -translate-y-1/2 bg-[#333] duration-300"
+            :class="{ 'opacity-0': isMenuOpen }"
           ></span>
           <span
-            class="absolute bottom-3 left-1/2 h-0.5 w-6 -translate-x-1/2 bg-[#333333]"
+            class="absolute left-1/2 h-0.5 w-6 -translate-x-1/2 bg-[#333] duration-300"
+            :class="
+              isMenuOpen ? 'bottom-1/2  translate-y-1/2 -rotate-45' : 'bottom-3'
+            "
           ></span>
         </button>
       </div>
