cda477b6f84999216fb00ccdf7c804106cc1afde
rousong <recall131477@gmail.com>
2023-08-26 11:52:03 +0800

    fix: base to vercel bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/realworld/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
