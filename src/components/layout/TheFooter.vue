5ed0997471eb7d53732deb90e7bfbf63ceef8c3b
rousong <recall131477@gmail.com>
2023-09-04 23:38:47 +0800

    refactor: layout header and footer file architecture

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  name: 'TheFooter',
+};
+</script>
+
+<template>
+  <footer class="bg-[#f3f3f3] py-4">
+    <div class="mx-auto max-w-[1140px] px-[15px]">
+      <a href="javascript:;" class="font-titillium text-primary"> conduit </a>
+      <span class="ml-2.5 text-sm font-light text-[#bbb]"
+        >An interactive learning project from Thinkster. Code & design licensed
+        under MIT.</span
+      >
+    </div>
+  </footer>
+</template>
